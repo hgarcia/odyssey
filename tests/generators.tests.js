cadfe7b5c639ec0a4e70734a91b129b12af2b599
Hernan Garcia <hernan@dynamicprogrammer.com>
2013-02-02 00:52:01 -0500

    -mTesting generators.

--- tests/generators.tests.js
+++ tests/generators.tests.js
@@ -1,5 +1,6 @@
 var should = require("should");
 var sinon = require("sinon");
+var fs = require('fs');
 var generators = require("../bin/engines/generators");
 var cnsMock = {
   content: "",
@@ -7,16 +8,42 @@ var cnsMock = {
     this.content += txt;
   }
 };
+// var fsMock = {
+//   writeFileName: "",
+//   writeContent: "",
+//   writeEncoding: "",
+//   readContents: {},
+//   reset: function () {
+//     this.writeFileName = "";
+//     this.writeContent = "";
+//     this.writeEncoding = "";
+//     this.readContents = {};
+//   },
+//   writeFileSync: function (fileName, content, encoding) {
+//     this.writeFileName = fileName;
+//     this.writeContent = content;
+//     this.writeEncoding = encoding;
+//   },
+//   readFileSync: function (fileName, encoding) {
+//     return this.readContents[fileName];
+//   }
+// };
 
 describe("generators", function() {
+
+  beforeEach(function () {
+    sinon.stub(fs, 'writeFileSync', function (fileName, content, encoding) { });
+  });
+
+  afterEach(function () {
+    fs.writeFileSync.restore();
+  });
 
   describe(".init(['new', 'first-migration'])", function () {
     it("should create a new migration file", function () {
       var args = ["new", "first-migration"];
-      // var fsMock = sinon;
-      cnsMock.content = "";
-      generators.init(args, fsMock, cnsMock)
+      generators.init(args, fs, cnsMock);
+
     });
   });
-
 });
